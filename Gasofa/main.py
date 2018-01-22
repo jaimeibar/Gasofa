1b2c63c578b80d053395ce789ce3d5fe93b6d28e
Jaime Ibar <jim2k7@gmail.com>
2018-01-22 16:03:39 +0000

    Layout moved to main.kv file. Updated references to widgets

--- Gasofa/main.py
+++ Gasofa/main.py
@@ -5,9 +5,6 @@ from __future__ import print_function
 import kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
 from kivy.logger import Logger
 
 
@@ -18,37 +15,6 @@ __version__ = '0.1'
 
 class MainScreen(GridLayout):
 
-    def __init__(self, **kwargs):
-        super(MainScreen, self).__init__(**kwargs)
-        self.cols = 2
-
-        self.add_widget(Label(text='Kms'))
-        self.input_kmtrs = TextInput(multiline=False, input_filter='float', write_tab=False)
-        self.add_widget(self.input_kmtrs)
-
-        self.add_widget(Label(text='€/l'))
-        self.input_price = TextInput(multiline=False, input_filter='float', write_tab=False)
-        self.add_widget(self.input_price)
-
-        self.add_widget(Label(text='l/100'))
-        self.input_avgconsumption = TextInput(multiline=False, input_filter='float', write_tab=False)
-        self.add_widget(self.input_avgconsumption)
-
-        self.add_widget(Label(text='Litres'))
-        self.input_litres = TextInput(multiline=False, input_filter='int', disabled=True, readonly=True, is_focusable=False)
-        self.add_widget(self.input_litres)
-
-        self.add_widget(Label(text='€'))
-        self.input_euros = TextInput(multiline=False, input='float', disabled=True, readonly=True, is_focusable=False)
-        self.add_widget(self.input_euros)
-
-        btn_go = Button(text='Go')
-        btn_go.bind(on_press=self.on_press_callback)
-        self.add_widget(btn_go)
-
-        btn_reset = Button(text='Reset')
-        btn_reset.bind(on_press=self.on_reset_callback)
-        self.add_widget(btn_reset)
 
     def on_press_callback(self, value):
         """
@@ -57,28 +23,28 @@ class MainScreen(GridLayout):
         :return:
         """
         ltrs = self.calculate_number_of_litres()
-        self.input_litres.text = str(ltrs)
         total = self.calculate_bill(ltrs)
-        self.input_euros.text = str(total)
+        self.ids.inputlitres.text = str(ltrs)
+        self.ids.inputeuros.text = str(total)
 
-    def on_reset_callback(self, value):
+    def on_clear_callback(self, value):
         """
         Clear all fields.
         :param value:
         :return:
         """
-        self.input_kmtrs.text = ''
-        self.input_price.text = ''
-        self.input_avgconsumption.text = ''
-        self.input_litres.text = ''
-        self.input_euros.text = ''
+        self.ids.inputkms.text = ''
+        self.ids.inputprice.text = ''
+        self.ids.inputavgconsumption.text = ''
+        self.ids.inputlitres.text = ''
+        self.ids.inputeuros.text = ''
 
     def calculate_number_of_litres(self):
         """
         Number of litres.
         :return: Number of litres
         """
-        litrs = (float(self.input_kmtrs.text) * float(self.input_avgconsumption.text)) / 100
+        litrs = (float(self.ids.inputkms.text) * float(self.ids.inputavgconsumption.text)) / 100
         return litrs
 
     def calculate_bill(self, litrs):
@@ -87,7 +53,7 @@ class MainScreen(GridLayout):
         :param litrs: Number of litres
         :return: Total amount in €
         """
-        total = float(litrs) * float(self.input_price.text)
+        total = float(litrs) * float(self.ids.inputprice.text)
         return total
 
 
@@ -100,4 +66,5 @@ class MainApp(App):
 
 
 if __name__ == '__main__':
+    Logger.info('Starting Gasofa')
     MainApp().run()
